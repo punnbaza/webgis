ea0b1e0c387d21a7cf2c59d21e4ebffc5d452cc3
punnbaza <85600706+punnbaza@users.noreply.github.com>
2021-06-23 03:39:35 +0700

    Update renderMap.js

--- renderMap.js
+++ renderMap.js
@@ -6,7 +6,29 @@ import {loadModules} from 'esri-loader'
 function Map(props) {
     const myId = props.myId;
     const MapEl = useRef(null);
-    console.log('choose: '+myId);
+    console.log('choose: '+ myId);
+
+    const template_point = {
+        title: "Attribute",
+        content: "RASTERVALU: {RASTERVALU}" 
+    };
+
+    const color_point = {
+        type: "simple",
+        symbol: {
+          type: "simple-marker",
+          color: "red",
+          size: "8px",
+          outline: {
+            color: "white"
+          }
+        },
+        visualVariables: [{
+          type: "size",
+
+        }]
+    };
+
 
     useEffect(
         ()=>{
@@ -16,7 +38,7 @@ function Map(props) {
             css:true
         }).then(([MapView,WebMap,GeoJSONLayer,WebStyleSymbol])=>{
             const webmap = new WebMap({
-                basemap: 'satellite'
+                basemap: 'topo-vector'
             })
             view = new MapView({
                 map: webmap,
@@ -24,106 +46,136 @@ function Map(props) {
                 zoom: 12,
                 container : MapEl.current
             })
-            
-            
-            
+
+
+
 
             if(myId === "June2020"){
-                
+
                 const geojsonLayer = new GeoJSONLayer({
-                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202006_lalong.geojson"
+                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202006_lalong.geojson",
+                    popupTemplate: template_point,
+                    renderer: color_point,
                             });
                 webmap.add(geojsonLayer)
-                
-                
+
+
             }
             else if(myId === "July2020"){
-                
+
                 const geojsonLayer = new GeoJSONLayer({
-                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202007_lalong.geojson"
+                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202007_lalong.geojson",
+                    popupTemplate: template_point,
+                    renderer: color_point,
                             });
                 webmap.add(geojsonLayer)  
             }    
             else if(myId === "August2020"){
-                
+
                 const geojsonLayer = new GeoJSONLayer({
-                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202008_lalong.geojson"
+                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202008_latlong.geojson",
+                    popupTemplate: template_point,
+                    renderer: color_point,
                             });
                 webmap.add(geojsonLayer)  
             }
             else if(myId === "September2020"){
-                
+
                 const geojsonLayer = new GeoJSONLayer({
-                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202009_lalong.geojson"
+                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202009_lalong.geojson",
+                    popupTemplate: template_point,
+                    renderer: color_point,
                             });
                 webmap.add(geojsonLayer)  
             }
             else if(myId === "October2020"){
-                
+
                 const geojsonLayer = new GeoJSONLayer({
-                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202010_lalong.geojson"
+                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202010_lalong.geojson",
+                    popupTemplate: template_point,
+                    renderer: color_point,
                             });
                 webmap.add(geojsonLayer)  
             }
             else if(myId === "November2020"){
-                
+
                 const geojsonLayer = new GeoJSONLayer({
-                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202011_lalong.geojson"
+                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202011_lalong.geojson",
+                    popupTemplate: template_point,
+                    renderer: color_point,
                             });
                 webmap.add(geojsonLayer)  
             }
             else if(myId === "December2020"){
-                
+
                 const geojsonLayer = new GeoJSONLayer({
-                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202012_lalong.geojson"
+                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202012_lalong.geojson",
+                    popupTemplate: template_point,
+                    renderer: color_point,
                             });
                 webmap.add(geojsonLayer)  
             }
             else if(myId === "January2021"){
-                
+
                 const geojsonLayer = new GeoJSONLayer({
-                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202101_lalong.geojson"
+                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202101_lalong.geojson",
+                    popupTemplate: template_point,
+                    renderer: color_point,
                             });
                 webmap.add(geojsonLayer)  
             }
             else if(myId === "February2021"){
-                
+
                 const geojsonLayer = new GeoJSONLayer({
-                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202102_lalong.geojson"
+                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202102_lalong.geojson",
+                    popupTemplate: template_point,
+                    renderer: color_point,
                             });
                 webmap.add(geojsonLayer)  
             }
             else if(myId === "March2021"){
-                
+
                 const geojsonLayer = new GeoJSONLayer({
-                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202103_lalong.geojson"
+                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202103_lalong.geojson",
+                    popupTemplate: template_point,
+                    renderer: color_point,
                             });
                 webmap.add(geojsonLayer)  
             } 
             else if(myId === "April2021"){
-                
+
                 const geojsonLayer = new GeoJSONLayer({
-                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202104_lalong.geojson"
-                    
+                    url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/202104_lalong.geojson",
+                    popupTemplate: template_point,
+                    renderer: color_point,
+
                             });
                 webmap.add(geojsonLayer)  
             }
-                                 
+
             else{
-                
-            
+
+
             }
-            
-            
+
+
             const addpipe = new GeoJSONLayer({
                 url: "https://raw.githubusercontent.com/chtoeii/Geojson-Data/main/pipe_line.geojson",
-                
-                        });
+                renderer: {
+                    type: "simple",
+                    symbol: {
+                      type: "simple-fill",
+                      color: [0, 0, 0, 0],
+                      outline: {
+                        color: [47, 84, 129, 1],
+                        width: 0.8,
+                      }
+                    }
+                  }
+
+            });
+
             webmap.add(addpipe)
-
-            
-
-            
 
         })
         return()=>{
@@ -135,9 +187,10 @@ function Map(props) {
         })
     return (
         <div style = {{height:550}} ref = {MapEl}>
-                     
+
         </div>
     )
+
 }
 
 export default Map;
